211601de0c5a281f8768314f762304ce9d10aa1b
Digital Business & Technology Apfelkiste <digitalbusinesstechnology@apfelkiste.ch>
2025-11-08 11:24:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-792056918-383a7",
+  "appId": "1:318247136449:web:34695c81b9786bada53840",
+  "apiKey": "AIzaSyAZjAihMumMXw6fmM0GVC-sKz0l3amoKzM",
+  "authDomain": "studio-792056918-383a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "318247136449"
+};
